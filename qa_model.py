185177423d7e79f6c752a256859282b09cbf518c
Fei Yang <klxfeiyang@gmail.com>
2017-03-14 18:48:05 -0700

    first commit

---
+++ qa_model.py
@@ -0,0 +1,296 @@
+ from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import time
+import logging
+
+import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops.nn import bidirectional_dynamic_rnn, dynamic_rnn
+
+from evaluate import exact_match_score, f1_score
+
+logging.basicConfig(level=logging.INFO)
+
+
+def get_optimizer(opt):
+    if opt == "adam":
+        optfn = tf.train.AdamOptimizer
+    elif opt == "sgd":
+        optfn = tf.train.GradientDescentOptimizer
+    else:
+        assert (False)
+    return optfn
+
+
+class Encoder(object):
+    def __init__(self, size, vocab_dim):
+        self.size = size
+        self.vocab_dim = vocab_dim
+
+    def encode(self, inputs, masks, encoder_state_input):
+        """
+        In a generalized encode function, you pass in your inputs,
+        masks, and an initial
+        hidden state input into this function.
+
+        :param inputs: Symbolic representations of your input
+        :param masks: this is to make sure tf.nn.dynamic_rnn doesn't iterate
+                      through masked steps
+        :param encoder_state_input: (Optional) pass this as initial hidden state
+                                    to tf.nn.dynamic_rnn to build conditional representations
+        :return: an encoded representation of your input.
+                 It can be context-level representation, word-level representation,
+                 or both.
+        """
+        cell = tf.nn.rnn_cell.LSTMCell(1)
+        (fw_out, bw_out), _ = bidirectional_dynamic_rnn(cell, 
+                                                        cell,
+                                                        inp,
+                                                        srclen,
+                                                        scope=scope, 
+                                                        time_major=True, 
+                                                        dtype=dtypes.float32)
+        return
+
+
+class Decoder(object):
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def decode(self, knowledge_rep):
+        """
+        takes in a knowledge representation
+        and output a probability estimation over
+        all paragraph tokens on which token should be
+        the start of the answer span, and which should be
+        the end of the answer span.
+
+        :param knowledge_rep: it is a representation of the paragraph and question,
+                              decided by how you choose to implement the encoder
+        :return:
+        """
+        
+        return
+
+class QASystem(object):
+    def __init__(self, encoder, decoder, *args):
+        """
+        Initializes your System
+
+        :param encoder: an encoder that you constructed in train.py
+        :param decoder: a decoder that you constructed in train.py
+        :param args: pass in more arguments as needed
+        """
+
+        # ==== set up placeholder tokens ========
+        self.inputs_placeholder = tf.placeholder(tf.float32, shape=(None, FLAGS.max_length), name="x")
+        self.labels_placeholder = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+
+        # ==== assemble pieces ====
+        with tf.variable_scope("qa", initializer=tf.uniform_unit_scaling_initializer(1.0)):
+            self.setup_embeddings()
+            self.setup_system()
+            self.setup_loss()
+
+        # ==== set up training/updating procedure ====
+        pass
+
+
+    def setup_system(self):
+        """
+        After your modularized implementation of encoder and decoder
+        you should call various functions inside encoder, decoder here
+        to assemble your reading comprehension system!
+        :return:
+        """
+        raise NotImplementedError("Connect all parts of your system here!")
+
+
+    def setup_loss(self):
+        """
+        Set up your loss computation here
+        :return:
+        """
+        with vs.variable_scope("loss"):
+            pass
+
+    def setup_embeddings(self):
+        """
+        Loads distributed word representations based on placeholder tokens
+        :return:
+        """
+        with vs.variable_scope("embeddings"):
+            pass
+
+    def optimize(self, session, train_x, train_y):
+        """
+        Takes in actual data to optimize your model
+        This method is equivalent to a step() function
+        :return:
+        """
+        input_feed = {}
+
+        # fill in this feed_dictionary like:
+        # input_feed['train_x'] = train_x
+
+        output_feed = []
+
+        outputs = session.run(output_feed, input_feed)
+
+        return outputs
+
+    def test(self, session, valid_x, valid_y):
+        """
+        in here you should compute a cost for your validation set
+        and tune your hyperparameters according to the validation set performance
+        :return:
+        """
+        input_feed = {}
+
+        # fill in this feed_dictionary like:
+        # input_feed['valid_x'] = valid_x
+
+        output_feed = []
+
+        outputs = session.run(output_feed, input_feed)
+
+        return outputs
+
+    def decode(self, session, test_x):
+        """
+        Returns the probability distribution over different positions in the paragraph
+        so that other methods like self.answer() will be able to work properly
+        :return:
+        """
+        input_feed = {}
+
+        # fill in this feed_dictionary like:
+        # input_feed['test_x'] = test_x
+
+        output_feed = []
+
+        outputs = session.run(output_feed, input_feed)
+
+        return outputs
+
+    def answer(self, session, test_x):
+
+        yp, yp2 = self.decode(session, test_x)
+
+        a_s = np.argmax(yp, axis=1)
+        a_e = np.argmax(yp2, axis=1)
+
+        return (a_s, a_e)
+
+    def validate(self, sess, valid_dataset):
+        """
+        Iterate through the validation dataset and determine what
+        the validation cost is.
+
+        This method calls self.test() which explicitly calculates validation cost.
+
+        How you implement this function is dependent on how you design
+        your data iteration function
+
+        :return:
+        """
+        valid_cost = 0
+
+        for valid_x, valid_y in valid_dataset:
+          valid_cost = self.test(sess, valid_x, valid_y)
+
+
+        return valid_cost
+
+    def evaluate_answer(self, session, dataset, sample=100, log=False):
+        """
+        Evaluate the model's performance using the harmonic mean of F1 and Exact Match (EM)
+        with the set of true answer labels
+
+        This step actually takes quite some time. So we can only sample 100 examples
+        from either training or testing set.
+
+        :param session: session should always be centrally managed in train.py
+        :param dataset: a representation of our data, in some implementations, you can
+                        pass in multiple components (arguments) of one dataset to this function
+        :param sample: how many examples in dataset we look at
+        :param log: whether we print to std out stream
+        :return:
+        """
+
+        f1 = 0.
+        em = 0.
+
+        if log:
+            logging.info("F1: {}, EM: {}, for {} samples".format(f1, em, sample))
+
+        return f1, em
+
+    def train(self, session, dataset, train_dir):
+        """
+        Implement main training loop
+
+        TIPS:
+        You should also implement learning rate annealing (look into tf.train.exponential_decay)
+        Considering the long time to train, you should save your model per epoch.
+
+        More ambitious appoarch can include implement early stopping, or reload
+        previous models if they have higher performance than the current one
+
+        As suggested in the document, you should evaluate your training progress by
+        printing out information every fixed number of iterations.
+
+        We recommend you evaluate your model performance on F1 and EM instead of just
+        looking at the cost.
+
+        :param session: it should be passed in from train.py
+        :param dataset: a representation of our data, in some implementations, you can
+                        pass in multiple components (arguments) of one dataset to this function
+        :param train_dir: path to the directory where you should save the model checkpoint
+        :return:
+        """
+
+        # some free code to print out number of parameters in your model
+        # it's always good to check!
+        # you will also want to save your model parameters in train_dir
+        # so that you can use your trained model to make predictions, or
+        # even continue training
+
+        tic = time.time()
+        params = tf.trainable_variables()
+        num_params = sum(map(lambda t: np.prod(tf.shape(t.value()).eval()), params))
+        toc = time.time()
+        logging.info("Number of params: %d (retreival took %f secs)" % (num_params, toc - tic))
+
+        # def train_on_batch(self, sess, inputs_batch, labels_batch):
+        # """Perform one step of gradient descent on the provided batch of data.
+        # This version also returns the norm of gradients.
+        # """
+        # feed = self.create_feed_dict(inputs_batch, labels_batch=labels_batch)
+        # _, loss, grad_norm = sess.run([self.train_op, self.loss, self.grad_norm], feed_dict=feed)
+        # return loss, grad_norm
+
+        # def run_epoch(self, sess, train):
+        # prog = Progbar(target=1 + int(len(train) / self.config.batch_size))
+        # losses, grad_norms = [], []
+        # for i, batch in enumerate(minibatches(train, self.config.batch_size)):
+        #     loss, grad_norm = self.train_on_batch(sess, *batch)
+        #     losses.append(loss)
+        #     grad_norms.append(grad_norm)
+        #     prog.update(i + 1, [("train loss", loss)])
+
+        # return losses, grad_norms
+
+        # def fit(self, sess, train):
+        # losses, grad_norms = [], []
+        # for epoch in range(self.config.n_epochs):
+        #     logger.info("Epoch %d out of %d", epoch + 1, self.config.n_epochs)
+        #     loss, grad_norm = self.run_epoch(sess, train)
+        #     losses.append(loss)
+        #     grad_norms.append(grad_norm)
+
+        # return losses, grad_norms
